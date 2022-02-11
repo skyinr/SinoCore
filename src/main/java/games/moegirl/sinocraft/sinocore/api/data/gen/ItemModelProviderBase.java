9d7bdd67fb96154c61cac71a9a71a518f466053c
秋雨落 <i@rain.cx>
2022-02-11 09:25:46 +0800

    feat(ItemModelProviderBase): Tweak javadoc. Remove repeat method and variables.
    Signed-off-by: 秋雨落 <i@rain.cx>

---
+++ src/main/java/games/moegirl/sinocraft/sinocore/api/data/gen/ItemModelProviderBase.java
@@ -0,0 +1,114 @@
+package games.moegirl.sinocraft.sinocore.api.data.gen;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CrossbowItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.TieredItem;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Base item model provider.
+ *
+ * @author skyinr
+ */
+public class ItemModelProviderBase extends ItemModelProvider {  // qyl: Use FooBase for our base data providers.
+    public static final ResourceLocation GENERATED = new ResourceLocation("item/generated");
+    public static final ResourceLocation HANDHELD = new ResourceLocation("item/handheld");
+
+    public ItemModelProviderBase(DataGenerator generator, String modId, ExistingFileHelper exHelper) {
+        super(generator, modId, exHelper);
+        // qyl: We need not an additional modId variable. MC already have.
+    }
+
+    /**
+     * Register item model.
+     */
+    @Override
+    protected void registerModels() {
+        Set<Item> items = ForgeRegistries.ITEMS.getValues().stream()
+                .filter(i -> modid.equals(ForgeRegistries.ITEMS.getRegistryName().getNamespace()))  // skyinr: Item in mod filter.
+                .collect(Collectors.toSet());
+
+        registerItemBlock(items.stream()
+                .filter(i -> i instanceof BlockItem)
+                .map(i -> (BlockItem) i)
+                .collect(Collectors.toSet()));
+        registerItem(items);
+    }
+
+    /**
+     * Register item block model.
+     *
+     * @param itemBlocks Set of item block.
+     */
+    private void registerItemBlock(@NotNull Set<BlockItem> itemBlocks) {
+        itemBlocks.forEach(i -> withExistingParent(name(i),
+                modLoc("block/" + name(i))));   // qyl: Change prefix to mc builtin "modLoc".
+    }
+
+    /**
+     * Add handheld item model.
+     *
+     * @param name Name of items.
+     * @return Builders.
+     */
+    private ItemModelBuilder handheldItem(String name) {
+        return withExistingParent(name, HANDHELD)
+                .texture("layer0", modLoc("items/" + name));    // qyl: Change prefix to mc builtin "modLoc".
+    }
+
+    /**
+     * @see ItemModelProviderBase#handheldItem(String)
+     */
+    private ItemModelBuilder handheldItem(Item i) {
+        return handheldItem(name(i));
+    }
+
+    /**
+     * Add generated item model.
+     *
+     * @param name Name of items.
+     * @return item model builder
+     */
+    private ItemModelBuilder generatedItem(String name) {
+        return withExistingParent(name, GENERATED)
+                .texture("layer0", modLoc("items/" + name));    // qyl: Change prefix to mc builtin "modLoc".
+    }
+
+    /**
+     * @see ItemModelProviderBase#generatedItem(String)
+     */
+    private ItemModelBuilder generatedItem(Item i) {
+        return generatedItem(name(i));
+    }
+
+    /**
+     * get item name
+     *
+     * @param item item
+     * @return item name
+     */
+    private static String name(Item item) {
+        return Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(item)).getPath();
+    }
+
+    private void registerItem(Set<Item> items) {
+        items.removeAll(items.stream()
+                .filter(item -> item instanceof BlockItem || item instanceof CrossbowItem)
+                .collect(Collectors.toSet()));  // skyinr: Block and crossbow filter.
+        items.forEach(this::generatedItem);
+        items.stream()
+                .filter(item -> item instanceof TieredItem)
+                .forEach(this::handheldItem);  // skyinr: Tiered items filter.
+    }
+}
