0f22417614db099472d31af3d0a38c54574b8572
luqin2007 <lq2007lq@hotmail.com>
2022-02-11 22:49:52 +0800

    feat: copy Heat capability from SinoCraft

---
+++ src/main/java/games/moegirl/sinocraft/sinocore/api/capability/ModCapabilities.java
@@ -0,0 +1,14 @@
+package games.moegirl.sinocraft.sinocore.api.capability;
+
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityManager;
+import net.minecraftforge.common.capabilities.CapabilityToken;
+
+/**
+ * All capability
+ */
+public class ModCapabilities {
+
+    public static final Capability<IHeat> HEAT = CapabilityManager.get(new CapabilityToken<>() {
+    });
+}
