67a6f649dc4e96d6e6d16fa0c5e9a5a9c53cec51
秋雨落 <i@rain.cx>
2022-04-13 21:50:55 +0800

    feat: ItemTagsProvider.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/api/java/games/moegirl/sinocraft/sinocore/api/data/BlockTagsProviderBase.java
+++ src/api/java/games/moegirl/sinocraft/sinocore/api/data/BlockTagsProviderBase.java
@@ -1,18 +1,31 @@
 package games.moegirl.sinocraft.sinocore.api.data;
 
+import net.minecraft.core.Registry;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BlockTagsProvider;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagManager;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
 
+import java.nio.file.Path;
+
 /**
- * register block tags
+ * Register block tags
  *
  * @author skyinr
  */
 public abstract class BlockTagsProviderBase extends BlockTagsProvider {
+    protected String mainModId;
+
     public BlockTagsProviderBase(DataGenerator pGenerator, String modId, @Nullable ExistingFileHelper existingFileHelper) {
         super(pGenerator, modId, existingFileHelper);
+    }
+
+    public BlockTagsProviderBase(DataGenerator pGenerator, String modId, @Nullable ExistingFileHelper existingFileHelper, String mainModIdIn) {
+        super(pGenerator, modId, existingFileHelper);
+        mainModId = mainModIdIn;
     }
 
     @Override
@@ -36,4 +49,18 @@ public abstract class BlockTagsProviderBase extends BlockTagsProvider {
     public abstract void addIronTool();
     public abstract void addDiamondTool();
 
+    @Override
+    public String getName() {
+        return "Mod " + modId + " Block Tags";
+    }
+
+    @Override
+    protected Path getPath(ResourceLocation loc) {
+        var key = this.registry.key();
+        return this.generator.getOutputFolder().resolve("data/" + getMainModId() + "/" + TagManager.getTagDir(key) + "/" + loc.getPath() + ".json");
+    }
+
+    public String getMainModId() {
+        return mainModId != null ? mainModId : modId;
+    }
 }
